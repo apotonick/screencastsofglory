264451fb0bf9f723a7799ac04df197ed7b88b7d7
Stefan Sprenger <info@stefan-sprenger.com>
2011-10-12 16:54:46 +0200

    Use asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require_self
+//= require_tree .
+
 $(function() {
   $("a[data-event-url]").live("click", function(e) {
     $.ajax({url: $(this).attr("data-event-url")});
